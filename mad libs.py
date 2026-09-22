e8dc34f4045e9c1224a0e6e694d3e267e6faa59a
Em0tiCoN <atticus.bell@ucas-edu.net>
2026-09-22 08:51:39 -0600

    scdops

--- mad libs.py
+++ mad libs.py
@@ -1,3 +1,8 @@
 #Atticus Bell 7th mad libs
 
-print('On 1981 the U.S launched the very first ",  ,". it was named columbia and was piloted by two very brave ",  , "they had practiced" ,  , "for 2 years and were expert" ,  ,". columbia took off from" ,  , " using its powerful first stage ",  ," and soared off into" ,  , " blue" ,  , " at an altitude of " ,  , " feet, it went into orbit around the ",  ," ')
+noun=put somthing weird here idk im bad at coding
+pluralnoun= some code ima figure out
+verbendingining=code stuff
+
+
+print('On 1981 the U.S launched the very first ",  ,". it was named columbia and was piloted by two very brave ",  , "they had practiced" ,  , "for 2 years and were expert" ,  ,". columbia took off from" ,  , " using its powerful first stage ",  ," and soared off into" ,  , " blue" ,  , " at an altitude of " ,  , " feet, it went into orbit around the ",  ,". for prople watching from earth, it was an/a" ,  ," sight to " ,  , "!')
