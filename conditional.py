678f226eeac0185c137e29be134c12de4048b7fa
Em0tiCoN <atticus.bell@ucas-edu.net>
2026-09-22 14:53:05 -0600

    vzdvdcv

---
+++ conditional.py
@@ -0,0 +1,18 @@
+#conditions notes AB
+
+time=1416
+day= "tusday"
+
+if time < 1200 and time > 500:
+    print("good morning!")
+elif time < 1700:
+    if day !="saturday" and day !+ "sunday":
+    
+
+    print("good afternoon")
+elif time < 2000:
+    print("good evening")
+else:
+    print("good night")
+
+print("code is done")
