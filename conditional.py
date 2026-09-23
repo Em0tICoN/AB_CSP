b29af06cab9fdd70086cbc3faa45d816afee72f3
Em0tiCoN <atticus.bell@ucas-edu.net>
2026-09-23 14:53:12 -0600

    sdcasv fudge you

--- conditional.py
+++ conditional.py
@@ -6,10 +6,11 @@ day= "tusday"
 if time < 1200 and time > 500:
     print("good morning!")
 elif time < 1700:
-    if day !="saturday" and day !+ "sunday":
+    if day !="saturday" and day ! + "sunday":
     
 
     print("good afternoon")
+    
 elif time < 2000:
     print("good evening")
 else:
---
+++ password.py
@@ -0,0 +1,10 @@
+#AB 7th password checker
+
+password=input("What is your password: ")
+letter=False
+symbol=False
+upppercase=False
+lowercase=False
+number=False
+
+if letter is < 5 set to (True)
