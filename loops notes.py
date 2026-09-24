57c21cae8ecb15f09ef90773322d07f0d2c5341c
Em0tiCoN <atticus.bell@ucas-edu.net>
2026-09-24 14:53:43 -0600

    zcZS

--- loops notes.py
+++ loops notes.py
@@ -1,7 +1,22 @@
 #AB, Loops Notes
+import random
 
 count=1
 
-while count <= 1000:
+while count <= 1:
     print(count)
     count+=1
+
+ducks=1
+goose=random.randint(1,11)
+
+while True:
+    if ducks==goose:
+        break
+    print('duck....')
+    ducks=+1
+print('GOOSE!')
+
+
+sibilings=["alex", "kaitie", "andrew", "tia", "treyson", "xaivier", "jake"]
+print(sibilings)
