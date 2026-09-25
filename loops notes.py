3d8d6cef51fe69970ced9aa35eb5da5d6bbb08e4
Em0tiCoN <atticus.bell@ucas-edu.net>
2026-09-25 14:31:11 -0600

    eqw

--- loops notes.py
+++ loops notes.py
@@ -3,9 +3,10 @@ import random
 
 count=1
 
-while count <= 1:
+while count <= 100000000000000000000000000:
     print(count)
     count+=1
+
 
 ducks=1
 goose=random.randint(1,11)
@@ -19,4 +20,19 @@ print('GOOSE!')
 
 
 sibilings=["alex", "kaitie", "andrew", "tia", "treyson", "xaivier", "jake"]
+print(sibilings[2])
+sibilings.append("jeyshnee")
+sibilings.insert(3, "vienna")
 print(sibilings)
+sibilings.pop(3)
+print(sibilings)
+for sibilings in sibilings:
+    print(sibilings)
+
+for num in range(1,11,3):
+    print(num)
+
+for num in range(1,25):
+    if num% 15 == 0:
+        print("fizbuzz")
+    elif num
